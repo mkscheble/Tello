e3471af95fc17fcdc6fc0287e399538aabc7c3d2
Mark <mkscheble@berkeley.edu>
2020-12-02 19:10:04 -0800

    face tracking

--- tracking.py
+++ tracking.py
@@ -5,8 +5,14 @@ myDrone = initializeTello()
 width, height = 360, 240
 
 while True:
+    #Flight
+    if start == 0:
+        myDrone.takeoff()
+        start = 1
     # Step 1 get the frame and show the image
     img = telloGetFrame(myDrone, width, height)
+    img, info = findFace(img)
+    pError = trackFace(myDrone, info, width, pid, pError)
     cv2.imshow('Image', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- utils.py
+++ utils.py
@@ -21,4 +21,54 @@ def telloGetFrame(myDrone, width = 360, height = 240):
     img = cv2.resize(myFrame, (width, height))
     return img
 
+def trackFace(myDrone, info, w, pid, pError):
+    ##PID
+    kp = pid[0]
+    kd = pid[1]
+    error = info[0][0] - w//2
+    speed = kp*error + kd*(error- pError) #need to add pi part
+    speed = int(np.clip(speed, -100, 100)) #can't have too fast of speed
+    print(speed)
+
+    if info[0][0] != 0:
+        myDrone.yaw_velocity = speed
+    else:
+        myDrone.for_back_velocity = 0
+        myDrone.left_right_right = 0
+        myDrone.up_down_velocity = 0
+        myDrone.yaw_velocity = 0
+        error = 0
+    if myDrone.send_rc_control:
+        myDrone.send_rc_control(myDrone.left_right_velocity,
+                                myDrone.for_back_velocity,
+                                myDrone.up_down_velocity,
+                                myDrone.yaw_velocity)
+
+
+    return error
+
+
+def findFace(img):
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 6)
+
+    myFaceListC = []
+    myFaceListArea = []
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cx = x + w // 2
+        cy = y + h // 2
+        area = w * h
+        myFaceListArea.append(area)
+        myFaceListC.append([cx, cy])
+
+    if len(myFaceListArea) != 0:
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i], myFaceListArea[i]]
+    else:
+        return img, [[0, 0], 0]
+
+
 
