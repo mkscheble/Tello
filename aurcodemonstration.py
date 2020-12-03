e1a5be311313d94047501de5f6ce75acf7772756
mkscheble <mkscheble@yahoo.com>
2020-12-03 15:15:48 -0800

    working data queue with q for command escape

--- aurcodemonstration.py
+++ aurcodemonstration.py
@@ -7,15 +7,15 @@ import numpy as np
 # markerImage = cv2.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
 # cv2.imwrite("marker33.png", markerImage)
 img = cv2.imread('marker33.png')
-cv2.imshow("marker", img)
-cv2.waitKey(0)
+# cv2.imshow("marker", img)
+# cv2.waitKey(0)
 ##detecting markers
 
-# dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
-# parameters = cv.aruco.DetectorParameters_create()
-# #have to set frame to something
-# frame = imread("marker33.png"")
-# markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(frame, dictionary, parameters = parameters)
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+parameters = cv2.aruco.DetectorParameters_create()
+#have to set frame to something
+markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters = parameters)
+
 
 
 
