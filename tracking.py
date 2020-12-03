cbd802458825a840579f0fded7dd1e3b23cf5030
Mark <mkscheble@berkeley.edu>
2020-12-02 19:14:05 -0800

    face tracking

--- tracking.py
+++ tracking.py
@@ -1,20 +1,28 @@
 from utils import *
 import cv2
 
+w, h = 360, 240
+pid = [0.4, 0.4, 0]
+pError = 0
+startCounter = 0  # for no Flight 1   - for flight 0
+
 myDrone = initializeTello()
-width, height = 360, 240
 
 while True:
-    #Flight
-    if start == 0:
-        myDrone.takeoff()
-        start = 1
-    # Step 1 get the frame and show the image
-    img = telloGetFrame(myDrone, width, height)
-    img, info = findFace(img)
-    pError = trackFace(myDrone, info, width, pid, pError)
-    cv2.imshow('Image', img)
 
+    ## Flight
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
+
+    ## Step 1
+    img = telloGetFrame(myDrone, w, h)
+    ## Step 2
+    img, info = findFace(img)
+    ## Step 3
+    pError = trackFace(myDrone, info, w, pid, pError)
+    # print(info[0][0])
+    cv2.imshow('Image', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         myDrone.land()
         break
--- utils.py
+++ utils.py
@@ -1,51 +1,27 @@
 from djitellopy import Tello
-from cv2 import cv2
+import cv2
+import numpy as np
+
 
 def initializeTello():
     myDrone = Tello()
     myDrone.connect()
     myDrone.for_back_velocity = 0
-    myDrone.left_right_right = 0
+    myDrone.left_right_velocity = 0
     myDrone.up_down_velocity = 0
     myDrone.yaw_velocity = 0
     myDrone.speed = 0
     print(myDrone.get_battery())
-    print("\n")
     myDrone.streamoff()
     myDrone.streamon()
     return myDrone
 
-def telloGetFrame(myDrone, width = 360, height = 240):
+
+def telloGetFrame(myDrone, w=360, h=240):
     myFrame = myDrone.get_frame_read()
     myFrame = myFrame.frame
-    img = cv2.resize(myFrame, (width, height))
+    img = cv2.resize(myFrame, (w, h))
     return img
-
-def trackFace(myDrone, info, w, pid, pError):
-    ##PID
-    kp = pid[0]
-    kd = pid[1]
-    error = info[0][0] - w//2
-    speed = kp*error + kd*(error- pError) #need to add pi part
-    speed = int(np.clip(speed, -100, 100)) #can't have too fast of speed
-    print(speed)
-
-    if info[0][0] != 0:
-        myDrone.yaw_velocity = speed
-    else:
-        myDrone.for_back_velocity = 0
-        myDrone.left_right_right = 0
-        myDrone.up_down_velocity = 0
-        myDrone.yaw_velocity = 0
-        error = 0
-    if myDrone.send_rc_control:
-        myDrone.send_rc_control(myDrone.left_right_velocity,
-                                myDrone.for_back_velocity,
-                                myDrone.up_down_velocity,
-                                myDrone.yaw_velocity)
-
-
-    return error
 
 
 def findFace(img):
@@ -71,4 +47,23 @@ def findFace(img):
         return img, [[0, 0], 0]
 
 
-
+def trackFace(myDrone, info, w, pid, pError):
+    ## PID
+    error = info[0][0] - w // 2
+    speed = pid[0] * error + pid[1] * (error - pError)
+    speed = int(np.clip(speed, -100, 100))
+    print(speed)
+    if info[0][0] != 0:
+        myDrone.yaw_velocity = speed
+    else:
+        myDrone.for_back_velocity = 0
+        myDrone.left_right_velocity = 0
+        myDrone.up_down_velocity = 0
+        myDrone.yaw_velocity = 0
+        error = 0
+    if myDrone.send_rc_control:
+        myDrone.send_rc_control(myDrone.left_right_velocity,
+                                myDrone.for_back_velocity,
+                                myDrone.up_down_velocity,
+                                myDrone.yaw_velocity)
+    return error
