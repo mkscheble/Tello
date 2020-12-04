588a248e058bba89b5e52be66d4e0b538454c826
mkscheble <mkscheble@yahoo.com>
2020-12-03 16:23:16 -0800

    sensing aruco

--- aurcodemonstration.py
+++ aurcodemonstration.py
@@ -6,8 +6,10 @@ import numpy as np
 # markerImage = np.zeros((200, 200), dtype=np.uint8)
 # markerImage = cv2.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
 # cv2.imwrite("marker33.png", markerImage)
-img = cv2.imread('marker33.png')
-# cv2.imshow("marker", img)
+img = cv2.imread('IMG_1597.jpg')
+img = cv2.resize(img, (640, 480))
+cv2.imshow("marker", img)
+cv2.waitKey()
 
 ##detecting markers
 
