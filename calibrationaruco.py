9b95b6ccd6f14fa9a9efcdca2528cbe1d57233b1
Mark <mkscheble@berkeley.edu>
2020-12-03 21:39:39 -0800

    trying to calibrate

---
+++ calibrationaruco.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2, PIL, os
+from cv2 import aruco
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+%matplotlib nbagg
