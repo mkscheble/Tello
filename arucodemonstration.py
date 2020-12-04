9a6553e54846bb72f5552b545af7aa1855e454ed
mkscheble <mkscheble@yahoo.com>
2020-12-04 08:50:47 -0800

    with calibration constants

--- arucodemonstration.py
+++ arucodemonstration.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-# dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 
-# markerImage = np.zeros((200, 200), dtype=np.uint8)
-# markerImage = cv2.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
+markerImage = np.zeros((200, 200), dtype=np.uint8)
+markerImage = cv2.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
 # cv2.imwrite("marker33.png", markerImage)
 img = cv2.imread('IMG_1597.jpg')
 img = cv2.resize(img, (640, 480))
@@ -13,14 +13,14 @@ cv2.waitKey()
 
 ##detecting markers
 
-dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-parameters = cv2.aruco.DetectorParameters_create()
-#have to set frame to something
-markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters = parameters)
-frame_markers = cv2.aruco.drawDetectedMarkers(img.copy(), markerCorners, markerIds)
-print(markerIds)
-cv2.imshow('markers', frame_markers)
-cv2.waitKey()
+# dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+# parameters = cv2.aruco.DetectorParameters_create()
+# #have to set frame to something
+# markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters = parameters)
+# frame_markers = cv2.aruco.drawDetectedMarkers(img.copy(), markerCorners, markerIds)
+# print(markerIds)
+# cv2.imshow('markers', frame_markers)
+# cv2.waitKey()
 
 
 
