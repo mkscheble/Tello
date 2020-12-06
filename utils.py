b972d2f100d53346acd20f650ccae2f9163dd25b
mkscheble <mkscheble@yahoo.com>
2020-12-06 14:17:32 -0800

    need to write two pids

--- utils.py
+++ utils.py
@@ -188,7 +188,7 @@ def trackAruco(myDrone, twist, pid, pid2, pid3, pError, pError2, pError3, iError
     # change these values for world coordinates (meters) where you want to be
     xdesired = 0
     ydesired = -0.03
-    zdesired = 0.3
+    zdesired = 0.6
 
     # Sends RC command based on distance of translation vector
     if np.all(twist[1]) != None:
